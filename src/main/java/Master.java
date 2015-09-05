46f0ad6387a77e4b0e651390919dae97bc25395a
matsu-chara <matsuy00@gmail.com>
2015-09-06 01:24:55 +0900

    add master

---
+++ src/main/java/Master.java
@@ -0,0 +1,29 @@
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+public class Master implements Watcher {
+    ZooKeeper zk;
+    String hostPort;
+
+    public Master(String hostPort) {
+        this.hostPort = hostPort;
+    }
+
+    void startZk() throws IOException {
+        zk = new ZooKeeper(hostPort, 15000, this);
+    }
+
+    @Override public void process(WatchedEvent event) {
+        System.out.println(event);
+    }
+
+    public static void main(String args[]) throws Exception {
+        Master m = new Master(args[0]);
+        m.startZk();
+
+        Thread.sleep(60000);
+    }
+}
--- src/main/java/Watcher.java
+++
@@ -1,5 +0,0 @@
-import org.apache.zookeeper.WatchedEvent;
-
-public interface Watcher {
-    void process(WatchedEvent event);
-}
