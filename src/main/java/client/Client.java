818de3e8ff80061bb239547b3743020b68cc4ea9
matsu-chara <matsuy00@gmail.com>
2015-09-06 16:54:00 +0900

    add client

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,49 @@
+package client;
+
+import base.PrintWatcher;
+import base.ZookeeperExecutor;
+import base.ZookeeperRoleBase;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.KeeperException.*;
+
+import java.util.UUID;
+
+public class Client extends ZookeeperRoleBase {
+
+    public Client(ZooKeeper zk) {
+        super(zk);
+    }
+
+    public static void main(String[] args) throws Exception {
+        ZookeeperExecutor exec    = new ZookeeperExecutor();
+        PrintWatcher      watcher = new PrintWatcher();
+
+        exec.withZk(watcher, zk -> {
+            Client c = new Client(zk);
+            c.queueCommand(UUID.randomUUID().toString());
+            c.sleep(6);
+        });
+
+    }
+
+    String queueCommand(String command) {
+        while (true) {
+            try {
+                return zk.create("/tasks/task-",
+                                        command.getBytes(),
+                                        Ids.OPEN_ACL_UNSAFE,
+                                        CreateMode.PERSISTENT_SEQUENTIAL);
+            } catch (NodeExistsException e) {
+                throw new RuntimeException(e.getPath() + " already appears to be running");
+            } catch (ConnectionLossException ignored) {
+
+            } catch (KeeperException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+}
